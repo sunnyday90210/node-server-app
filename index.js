0ddf43ed02b7ae5a2c34a5da97702a2d6424ddbd
Robert Wollny <robertjohnwollny@gmail.com>
2019-03-26 22:09:52 -0700

    :rocket:

--- index.js
+++ index.js
@@ -65,8 +65,10 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(1337, '127.0.0.1', () => {
-  console.log('Listening for requests now');
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`Server started on ${port}`);
 });
 
 function replaceTemplate(originalHtml, laptop) {
